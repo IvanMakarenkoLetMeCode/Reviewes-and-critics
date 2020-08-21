a1a29bcb0c8c84ee669a32d2c87378cc421efc87
IvanMakarenkoLetMeCode <ivan.makarenko@letmecode.org>
2020-08-21 11:14:40 +0300

    create-project

---
+++ Reviewes and critics/Reviewes and critics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reviewes and critics
+//
+//  Created by Ivan on 21.08.2020.
+//  Copyright © 2020 Ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
